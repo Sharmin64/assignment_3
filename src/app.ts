b60a751188604d008199f7c4bea42c9ac2b3b59d
Sharmin64 <lizaeatsbanana@gmail.com>
2024-06-13 23:27:52 +0600

    middlewares created

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import globalErrorHandler from "./app/mddlewares/globalErrorHandler";
+import notFoundRoute from "./app/mddlewares/notFoundRoute";
 const app: Application = express();
-// const port = 3000;
+
+// parsers
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use(globalErrorHandler);
+
+app.use(notFoundRoute);
 
 export default app;
--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,14 +1,19 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
 
-const userSchema = new Schema<TUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phone: { type: String, required: true },
-  role: { type: String, enum: ["admin", "user"], required: true },
-  address: { type: String, required: true },
-});
+const userSchema = new Schema<TUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], required: true },
+    address: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Create and export the Mongoose model
 export const User = model<TUser>("User", userSchema);
